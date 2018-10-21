a7a0e17247dbf2f4ad89bfa93446a3edb676b7b1
jklancic <jernej.klancic@appdirect.com>
2018-10-21 13:04:07 -0600

    Fixed runtime errors

--- src/main/java/xyz/blackmonster/BitCoinMonitor.java
+++ src/main/java/xyz/blackmonster/BitCoinMonitor.java
@@ -6,6 +6,7 @@ import org.apache.storm.topology.TopologyBuilder;
 
 import xyz.blackmonster.bolts.CriticalLowValue;
 import xyz.blackmonster.bolts.CriticalTopValue;
+import xyz.blackmonster.bolts.LogValue;
 import xyz.blackmonster.spouts.PriceEmitter;
 
 /**
@@ -14,19 +15,22 @@ import xyz.blackmonster.spouts.PriceEmitter;
 public class BitCoinMonitor {
 
 	public static final String PRICE_EMITTER = "price-emitter";
+	public static final String LOG_VALUE = "log-value";
 
 	public static void main(String[] args) throws InterruptedException {
 		TopologyBuilder builder = new TopologyBuilder();
 		builder.setSpout(PRICE_EMITTER, new PriceEmitter());
-		builder.setBolt("critical-low", new CriticalLowValue()).shuffleGrouping(PRICE_EMITTER);
-		builder.setBolt("critical-top", new CriticalTopValue()).shuffleGrouping(PRICE_EMITTER);
+
+		builder.setBolt(LOG_VALUE, new LogValue()).shuffleGrouping(PRICE_EMITTER);
+		builder.setBolt("critical-low", new CriticalLowValue()).shuffleGrouping(LOG_VALUE);
+		builder.setBolt("critical-top", new CriticalTopValue()).shuffleGrouping(LOG_VALUE);
 
 		// Configuration
 		Config config = new Config();
-		config.put(PriceEmitter.INTERVAL_LENGTH_CONF, "60000");
+		config.put(PriceEmitter.INTERVAL_LENGTH_CONF, new Integer(60000));
 		config.put(PriceEmitter.URL_CONF, "https://api.coindesk.com/v1/bpi/currentprice.json");
-		config.put(CriticalLowValue.BOTTOM_LIMIT_CONF, "6000");
-		config.put(CriticalTopValue.TOP_LIMIT_CONF, "6200");
+		config.put(CriticalLowValue.BOTTOM_LIMIT_CONF, new Float(6000));
+		config.put(CriticalTopValue.TOP_LIMIT_CONF, new Float(6200));
 		config.setDebug(false);
 
 		// Run topology
---
+++ src/main/java/xyz/blackmonster/bolts/LogValue.java
@@ -0,0 +1,28 @@
+package xyz.blackmonster.bolts;
+
+import org.apache.storm.topology.BasicOutputCollector;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseBasicBolt;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+
+import xyz.blackmonster.spouts.PriceEmitter;
+
+/**
+ * Logs every value
+ */
+public class LogValue extends BaseBasicBolt {
+
+	@Override
+	public void execute(Tuple input, BasicOutputCollector collector) {
+		float value = input.getFloatByField(PriceEmitter.PRICE);
+		System.out.println("Current value is: " + value);
+		collector.emit(new Values(value));
+	}
+
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		declarer.declare(new Fields(PriceEmitter.PRICE));
+	}
+}
